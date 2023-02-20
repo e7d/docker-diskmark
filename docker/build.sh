5d4bdb1962d683f37da979a8b19a040ecd629bb5
e7d <michael@e7d.io>
2023-02-20 15:42:16 +0100

    add docker build related files

---
+++ docker/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DIR=$(realpath $(dirname "$0")/../)
+docker buildx create --name diskmark --use
+docker buildx build \
+    --cache-from "type=local,src=$DIR/docker/cache" \
+    --cache-to "type=local,dest=$DIR/docker/cache" \
+    --file $DIR/Dockerfile \
+    --metadata-file $DIR/docker/build/metadata.json \
+    --output "type=registry" \
+    --platform=linux/amd64,linux/arm/v7,linux/arm64/v8,linux/ppc64le,linux/s390x \
+    --tag e7db/diskmark \
+    $DIR
+docker buildx rm diskmark
