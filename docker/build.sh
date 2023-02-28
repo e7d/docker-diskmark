b5782f4f6cccb37e8624076463e84d04f546a076
e7d <michael@e7d.io>
2023-02-28 17:40:36 +0100

    build adjutments

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 DIR=$(realpath $(dirname "$0")/../)
-docker buildx create --name diskmark --use
+mkdir -p $DIR/docker/build $DIR/docker/cache
+docker buildx create --name e7db-diskmark --use
 docker buildx build \
     --cache-from "type=local,src=$DIR/docker/cache" \
     --cache-to "type=local,dest=$DIR/docker/cache" \
@@ -11,4 +12,4 @@ docker buildx build \
     --platform=linux/amd64,linux/arm/v7,linux/arm64/v8,linux/ppc64le,linux/s390x \
     --tag e7db/diskmark \
     $DIR
-docker buildx rm diskmark
+docker buildx rm e7db-diskmark
