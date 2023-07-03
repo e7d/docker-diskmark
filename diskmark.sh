7a4f351ef9603c5002054b5d68fe0e4e21410afd
e7d <michael@e7d.io>
2023-07-03 11:43:13 +0200

    code improvment and simplification

--- diskmark.sh
+++ diskmark.sh
@@ -18,143 +18,111 @@ function color() {
   echo "\e[$1$2"
 }
 
-function finally() {
-  local EXIT_CODE="${1:-0}"
-  echo
-  echo -e "❌ Benchmark $(color $BOLD $RED)failed$(color $RESET)."
-  if [ ! -z "$2" ]; then
-    echo "> $2"
+function clean() {
+  [[ -z $TARGET ]] && return
+  if [[ -n $ISNEWDIR ]]; then
+    rm -rf "$TARGET"
+  else
+    rm -f "$TARGET"/.diskmark.{json,tmp}
   fi
-  exit "${EXIT_CODE}"
 }
 
-trap 'finally $? "The benchmark was aborted before its completion."' INT
+function interrupt() {
+  local EXIT_CODE="${1:-0}"
+  echo -e "\r\n\n🛑 Benchmark $(color $BOLD $RED)interrupted$(color $RESET)."
+  if [ ! -z "$2" ]; then
+    echo "➤ $2"
+  fi
+  clean
+  exit "${EXIT_CODE}"
+}
+trap 'interrupt $? "The benchmark was aborted before its completion."' HUP INT QUIT KILL TERM
+
+function error() {
+  local EXIT_CODE="${1:-0}"
+  echo -e "\r\n\n❌ Benchmark $(color $BOLD $RED)failed$(color $RESET)."
+  if [ ! -z "$2" ]; then
+    echo "➤ $2"
+  fi
+  clean
+  exit "${EXIT_CODE}"
+}
+trap 'error $? "The benchmark failed before its completion."' ERR
 
 function toBytes() {
   local SIZE=$1
-  local UNIT=$(echo $SIZE | sed 's/[0-9]//g')
-  local NUMBER=$(echo $SIZE | sed 's/[a-zA-Z]//g')
+  local UNIT=${SIZE//[0-9]/}
+  local NUMBER=${SIZE//[a-zA-Z]/}
   case $UNIT in
-    G | g)
-      echo $((NUMBER*1024*1024*1024))
-      ;;
-    M | m)
-      echo $((NUMBER*1024*1024))
-      ;;
-    K | k)
-      echo $((NUMBER*1024))
-      ;;
-    *)
-      echo $NUMBER
-      ;;
+    T|t) echo $((NUMBER * 1024 * 1024 * 1024 * 1024));;
+    G|g) echo $((NUMBER * 1024 * 1024 * 1024));;
+    M|m) echo $((NUMBER * 1024 * 1024));;
+    K|k) echo $((NUMBER * 1024));;
+    *) echo $NUMBER;;
   esac
 }
 
-# convert bytes to human readable format
 function fromBytes() {
   local SIZE=$1
   local UNIT=""
-  if [ $SIZE -gt 1024 ]; then
-    SIZE=$((SIZE/1024))
+  if (( SIZE > 1024 )); then
+    SIZE=$((SIZE / 1024))
     UNIT="K"
   fi
-  if [ $SIZE -gt 1024 ]; then
-    SIZE=$((SIZE/1024))
+  if (( SIZE > 1024 )); then
+    SIZE=$((SIZE / 1024))
     UNIT="M"
   fi
-  if [ $SIZE -gt 1024 ]; then
-    SIZE=$((SIZE/1024))
+  if (( SIZE > 1024 )); then
+    SIZE=$((SIZE / 1024))
     UNIT="G"
   fi
   echo "${SIZE}${UNIT}"
 }
 
+function parseResult() {
+  local bandwidth=$(cat "$TARGET/.diskmark.json" | grep -A"$2" '"name" : "'"$1"'"' | grep "$3" | sed 's/        "'"$3"'" : //g' | sed 's:,::g' | awk '{ SUM += $1} END { printf "%.0f", SUM }')
+  local throughput=$(cat "$TARGET/.diskmark.json" | grep -A"$2" '"name" : "'"$1"'"' | grep "$4" | sed 's/        "'"$4"'" : //g' | sed 's:,::g' | awk '{ SUM += $1} END { printf "%.0f", SUM }' | cut -d. -f1)
+  echo "$(($bandwidth / 1024 / 1024)) MB/s, $throughput IO/s"
+}
+
 function parseReadResult() {
-  echo "$(($(cat "$TARGET/.diskmark.json" | grep -A15 '"name" : "'"$1"'"' | grep bw_bytes | cut -d: -f2 | sed s:,::g)/1024/1024)) MB/s, $(cat "$TARGET/.diskmark.json" | grep -A15 '"name" : "'"$1"'"' | grep -m1 iops | cut -d: -f2 | cut -d. -f1 | sed 's: ::g') IO/s"
+  parseResult "$1" 15 bw_bytes iops
 }
 
 function parseWriteResult() {
-  echo "$(($(cat "$TARGET/.diskmark.json" | grep -A80 '"name" : "'"$1"'"' | grep bw_bytes | sed '2!d' | cut -d: -f2 | sed s:,::g)/1024/1024)) MB/s, $(cat "$TARGET/.diskmark.json" | grep -A80 '"name" : "'"$1"'"' | grep iops | sed '7!d' | cut -d: -f2 | cut -d. -f1 | sed 's: ::g') IO/s"
+  parseResult "$1" 80 bw_bytes iops
 }
 
+
 function parseRandomReadResult() {
-  echo "$(($(cat "$TARGET/.diskmark.json" | grep -A15 '"name" : "'"$1"'"' | grep bw_bytes | sed 's/        "bw_bytes" : //g' | sed 's:,::g' | awk '{ SUM += $1} END { printf "%.0f", SUM }')/1024/1024)) MB/s, $(cat "$TARGET/.diskmark.json" | grep -A15 '"name" : "'"$1"'"' | grep iops | sed 's/        "iops" : //g' | sed 's:,::g' | awk '{ SUM += $1} END { printf "%.0f", SUM }' | cut -d. -f1) IO/s"
+  parseResult "$1" 15 bw_bytes iops
 }
 
 function parseRandomWriteResult() {
-  echo "$(($(cat "$TARGET/.diskmark.json" | grep -A80 '"name" : "'"$1"'"' | grep bw_bytes | sed 's/        "bw_bytes" : //g' | sed 's:,::g' | awk '{ SUM += $1} END { printf "%.0f", SUM }')/1024/1024)) MB/s, $(cat "$TARGET/.diskmark.json" | grep -A80 '"name" : "'"$1"'"' | grep iops | sed 's/        "iops" : //g' | sed 's:,::g' | awk '{ SUM += $1} END { printf "%.0f", SUM }' | cut -d. -f1) IO/s"
+  parseResult "$1" 80 bw_bytes iops
 }
 
 function loadDefaultProfile() {
-  NAME[0]="SEQ1MQ8T1"
-  LABEL[0]="Sequential 1M Q8T1"
-  BLOCKSIZE[0]="1M"
-  IODEPTH[0]=8
-  NUMJOBS[0]=1
-  READWRITE[0]=""
-  COLOR[0]=$(color $NORMAL $YELLOW)
-
-  NAME[1]="SEQ1MQ1T1"
-  LABEL[1]="Sequential 1M Q1T1"
-  BLOCKSIZE[1]="1M"
-  IODEPTH[1]=1
-  NUMJOBS[1]=1
-  READWRITE[1]=""
-  COLOR[1]=$(color $NORMAL $YELLOW)
-
-  NAME[2]="RND4KQ32T1"
-  LABEL[2]="Random 4K Q32T1"
-  BLOCKSIZE[2]="4k"
-  IODEPTH[2]=32
-  NUMJOBS[2]=1
-  READWRITE[2]="rand"
-  COLOR[2]=$(color $NORMAL $CYAN)
-  SIZEDIVIDER[2]=16
-
-  NAME[3]="RND4KQ1T1"
-  LABEL[3]="Random 4K Q1T1"
-  BLOCKSIZE[3]="4k"
-  IODEPTH[3]=1
-  NUMJOBS[3]=1
-  READWRITE[3]="rand"
-  COLOR[3]=$(color $NORMAL $CYAN)
-  SIZEDIVIDER[3]=32
+  NAME=("SEQ1MQ8T1" "SEQ1MQ1T1" "RND4KQ32T1" "RND4KQ1T1")
+  LABEL=("Sequential 1M Q8T1" "Sequential 1M Q1T1" "Random 4K Q32T1" "Random 4K Q1T1")
+  BLOCKSIZE=("1M" "1M" "4k" "4k")
+  IODEPTH=(8 1 32 1)
+  NUMJOBS=(1 1 1 1)
+  READWRITE=("" "" "rand" "rand")
+  COLOR=($(color $NORMAL $YELLOW) $(color $NORMAL $YELLOW) $(color $NORMAL $CYAN) $(color $NORMAL $CYAN))
+  SIZEDIVIDER=(-1 -1 16 32)
 }
 
 function loadNVMeProfile() {
-  NAME[0]="SEQ1MQ8T1"
-  LABEL[0]="Sequential 1M Q8T1"
-  BLOCKSIZE[0]="1M"
-  IODEPTH[0]=8
-  NUMJOBS[0]=1
-  READWRITE[0]=""
-  COLOR[0]=$(color $NORMAL $YELLOW)
-
-  NAME[1]="SEQ128KQ32T1"
-  LABEL[1]="Sequential 128K Q32T1"
-  BLOCKSIZE[1]="128k"
-  IODEPTH[1]=32
-  NUMJOBS[1]=1
-  READWRITE[1]=""
-  COLOR[1]=$(color $NORMAL $GREEN)
-
-  NAME[2]="RND4KQ32T16"
-  LABEL[2]="Random 4K Q32T16"
-  BLOCKSIZE[2]="4k"
-  IODEPTH[2]=32
-  NUMJOBS[2]=16
-  READWRITE[2]="rand"
-  COLOR[2]=$(color $NORMAL $CYAN)
-  SIZEDIVIDER[2]=16
-
-  NAME[3]="RND4KQ1T1"
-  LABEL[3]="Random 4K Q1T1"
-  BLOCKSIZE[3]="4k"
-  IODEPTH[3]=1
-  NUMJOBS[3]=1
-  READWRITE[3]="rand"
-  COLOR[3]=$(color $NORMAL $CYAN)
-  SIZEDIVIDER[3]=32
+  NAME=("SEQ1MQ8T1" "SEQ128KQ32T1" "RND4KQ32T16" "RND4KQ1T1")
+  LABEL=("Sequential 1M Q8T1" "Sequential 128K Q32T1" "Random 4K Q32T16" "Random 4K Q1T1")
+  BLOCKSIZE=("1M" "128k" "4k" "4k")
+  IODEPTH=(8 32 32 1)
+  NUMJOBS=(1 1 16 1)
+  READWRITE=("" "" "rand" "rand")
+  COLOR=($(color $NORMAL $YELLOW) $(color $NORMAL $GREEN) $(color $NORMAL $CYAN) $(color $NORMAL $CYAN))
+  SIZEDIVIDER=(-1 -1 16 32)
 }
 
 TARGET="${TARGET:-$(pwd)}"
@@ -178,7 +146,7 @@ if [ -z "$DRIVE" ]; then
   DRIVESIZE="unknown"
 else
   DRIVEMODEL=$(cat /sys/block/$DRIVE/device/model | sed 's/ *$//g')
-  DRIVESIZE=$(($(cat /sys/block/$DRIVE/size)*512/1024/1024/1024))GB
+  DRIVESIZE=$(($(cat /sys/block/$DRIVE/size) * 512 / 1024 / 1024 / 1024))GB
 fi
 case "$PROFILE" in
   default)
@@ -221,12 +189,15 @@ echo -e "$(color $BOLD $WHITE)Configuration:$(color $RESET)
 
 Benchmark is $(color $BOLD $WHITE)running$(color $RESET), please wait..."
 
-fio --loops=$LOOPS --size=$BYTESIZE --filename="$TARGET/.diskmark.tmp" --stonewall --ioengine=libaio --direct=1 --zero_buffers=$WRITEZERO --output-format=json \
-  --name=Bufread --loops=1 --blocksize=$BYTESIZE --iodepth=1 --numjobs=1 --readwrite=readwrite\
-  > /dev/null
+fio_benchmark() {
+  fio --loops="$LOOPS" --size="$1" --filename="$TARGET/.diskmark.tmp" --stonewall --ioengine=libaio --direct=1 --zero_buffers="$WRITEZERO" --output-format=json \
+    --name="$2" --blocksize="$3" --iodepth="$4" --numjobs="$5" --readwrite="$6" >"$TARGET/.diskmark.json"
+}
 
-for (( i=0; i<${#NAME[@]}; i++ )); do
-  TESTSIZE=$((${BYTESIZE}/${SIZEDIVIDER[$i]:-1}))
+fio_benchmark "$BYTESIZE" Bufread "$BYTESIZE" 1 1 readwrite >/dev/null
+
+for ((i = 0; i < ${#NAME[@]}; i++)); do
+  TESTSIZE=$((${BYTESIZE} / ${SIZEDIVIDER[$i]:-1}))
   case "${READWRITE[$i]}" in
     rand) PARSE="parseRandom" ;;
     *) PARSE="parse" ;;
@@ -235,22 +206,13 @@ for (( i=0; i<${#NAME[@]}; i++ )); do
   echo
   echo -e "${COLOR[$i]}${LABEL[$i]}:$(color $RESET)"
   printf "<= Read:  "
-  fio --loops=$LOOPS --size=${TESTSIZE} --filename="$TARGET/.diskmark.tmp" --stonewall --ioengine=libaio --direct=1 --zero_buffers=$WRITEZERO --output-format=json \
-    --name=${NAME[$i]}Read --blocksize=${BLOCKSIZE[$i]} --iodepth=${IODEPTH[$i]} --numjobs=${NUMJOBS[$i]} --readwrite=${READWRITE[$i]}read \
-    > "$TARGET/.diskmark.json"
+  fio_benchmark "$TESTSIZE" "${NAME[$i]}Read" "${BLOCKSIZE[$i]}" "${IODEPTH[$i]}" "${NUMJOBS[$i]}" "${READWRITE[$i]}read"
   echo "$(${PARSE}ReadResult "${NAME[$i]}Read")"
   printf "=> Write: "
-  fio --loops=$LOOPS --size=${TESTSIZE} --filename="$TARGET/.diskmark.tmp" --stonewall --ioengine=libaio --direct=1 --zero_buffers=$WRITEZERO --output-format=json \
-    --name=${NAME[$i]}Write --blocksize=${BLOCKSIZE[$i]} --iodepth=${IODEPTH[$i]} --numjobs=${NUMJOBS[$i]} --readwrite=${READWRITE[$i]}write \
-    > "$TARGET/.diskmark.json"
+  fio_benchmark "$TESTSIZE" "${NAME[$i]}Write" "${BLOCKSIZE[$i]}" "${IODEPTH[$i]}" "${NUMJOBS[$i]}" "${READWRITE[$i]}write"
   echo "$(${PARSE}WriteResult "${NAME[$i]}Write")"
 done
 
-echo
-echo -e "✅ Benchmark $(color $BOLD $GREEN)finished$(color $RESET)."
+echo -e "\n✅ Benchmark $(color $BOLD $GREEN)finished$(color $RESET)."
 
-if [ ! -z $ISNEWDIR ]; then
-  rm -rf "$TARGET"
-else
-  rm "$TARGET/.diskmark.json" "$TARGET/.diskmark.tmp"
-fi
+clean
